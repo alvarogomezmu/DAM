f24d0b35561dc4878cb2493c6d4860c42a72696f
alvarogomezmu <alvarog.m@hotmail.com>
2015-10-02 21:05:51 +0200

    2-10-2015

--- AccesoDatos/ProyectoAD/src/Herramientas/herramientas.java
+++ AccesoDatos/ProyectoAD/src/Herramientas/herramientas.java
@@ -2,8 +2,9 @@ package Herramientas;
 
 /**
  * @author Daniel Marcos Lorrio
- * @version 0.288 28 de Septiembre de 2015
+ * @version 1.0.1 02-10-2015
  */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,9 +16,9 @@ import java.util.StringTokenizer;
 public class herramientas {
 
     /**
-     * Metodo para recorrer listas
+     * Metodo que sirve para recorrer listas
      *
-     * @param colec Cualquier coleccion
+     * @param colec Para cualquier colección
      */
     public static void recorrerColeccion(Collection colec) {
         Iterator it = colec.iterator();
@@ -30,7 +31,7 @@ public class herramientas {
     /**
      * Metodo para recorrer maps con clave String
      *
-     * @param map Cualquier map
+     * @param map Para cualquier map
      */
     public static void recorrerMapStr(Map map) {
 
@@ -44,9 +45,9 @@ public class herramientas {
     }
 
     /**
-     * Metodo para recorrer maps con clave Integer
+     * Metodo para recorrer maps con clave Integer, de tipo entero
      *
-     * @param map Cualquier map
+     * @param map Para cualquier map
      */
     public static void recorrerMapInt(Map map) {
 
@@ -60,41 +61,40 @@ public class herramientas {
     }
 
     /**
-     * Determinar el tamaño de una cadena dentro de un fichero aleatorio
+     * Determina el tamaño de una cadena dentro de un fichero de tipo aleatorio
      *
-     * @param txt Cualquier string
-     * @param largo Integer asociado al string
-     * @return Cadena generada
+     * @param txt Para cualquier string
+     * @param longitud Integer que va asociado al string
+     * @return retorna la cadena generada
      */
-    public static String convertUTF(String txt, int largo) {
+    public static String convertUTF(String txt, int longitud) {
 
-        if (txt.length() > largo) {
-            return txt.substring(0, largo);
+        if (txt.length() > longitud) {
+            return txt.substring(0, longitud);
         }
-        for (int n = txt.length(); n < largo; n++) {
+        for (int n = txt.length(); n < longitud; n++) {
             txt = txt + " ";
         }
         return txt;
     }
-    //--------------------
 
     /**
      * Metodo para leer fichero mediante BufferedReader
      *
-     * @param br BufferedReader declarado previamente
-     * @param ar ArrayList de Integer declarado previamente
+     * @param lecturaFich BufferedReader declarado previamente
+     * @param arrList ArrayList de Integer declarado previamente
      */
-    public static void leerFichero(BufferedReader br, ArrayList<Integer> ar) {
+    public static void leerFichero(BufferedReader lecturaFich, ArrayList<Integer> arrList) {
         String linea = null;
         try {
-            while ((linea = br.readLine()) != null) {
+            while ((linea = lecturaFich.readLine()) != null) {
                 StringTokenizer tok = new StringTokenizer(linea, "!");
                 while (tok.hasMoreTokens()) {
-                    ar.add(Integer.parseInt(tok.nextToken()));
-                    ar.add(Integer.parseInt(tok.nextToken()));
+                    arrList.add(Integer.parseInt(tok.nextToken()));
+                    arrList.add(Integer.parseInt(tok.nextToken()));
                 }
             }
-            br.close();
+            lecturaFich.close();
         } catch (IOException e) {
             System.out.println("Error: " + e);
         }
@@ -103,25 +103,25 @@ public class herramientas {
     /**
      * Metodo para recorrer un ArrayList de Integers e imprimirlos
      *
-     * @param ar ArrayList de Integers declarado previamente
+     * @param arrList ArrayList de Integers declarado previamente
      */
-    public static void listarFichero(ArrayList<Integer> ar) {
-        for (int i = 0; i < ar.size(); i++) {
-            System.out.print(ar.get(i) + " ");
+    public static void listarFichero(ArrayList<Integer> arrList) {
+        for (int i = 0; i < arrList.size(); i++) {
+            System.out.print(arrList.get(i) + " ");
         }
     }
 
     /**
      * Metodo para contar los Integers de un ArrayList menores de 10
      *
-     * @param ar ArrayList de Integers declarado previamente
+     * @param arrList ArrayList de Integers declarado previamente
      * @return Cantidad de Integers menores de 10
      */
-    public static int contarUnaCifra(ArrayList<Integer> ar) {
+    public static int contarUnaCifra(ArrayList<Integer> arrList) {
         int count = 0;
 
-        for (int i = 0; i < ar.size(); i++) {
-            if (ar.get(i) < 10) {
+        for (int i = 0; i < arrList.size(); i++) {
+            if (arrList.get(i) < 10) {
                 count++;
             }
         }
@@ -131,14 +131,14 @@ public class herramientas {
     /**
      * Metodo para contar los Integers de un ArrayList mayores o igual a 10
      *
-     * @param ar ArrayList de Integers declarado previamente
+     * @param arrList ArrayList de Integers declarado previamente
      * @return Cantidad de Integers mayores o igual a 10
      */
-    public static int contarDosCifras(ArrayList<Integer> ar) {
+    public static int contarDosCifras(ArrayList<Integer> arrList) {
         int count = 0;
 
-        for (int i = 0; i < ar.size(); i++) {
-            if (ar.get(i) >= 10) {
+        for (int i = 0; i < arrList.size(); i++) {
+            if (arrList.get(i) >= 10) {
                 count++;
             }
         }
@@ -148,32 +148,32 @@ public class herramientas {
     /**
      * Metodo para sumar los Integers menores de 10 de un ArrayList
      *
-     * @param ar ArrayList de Integers declarado previamente
+     * @param arrList ArrayList de Integers declarado previamente
      * @return Suma de los integers
      */
-    public static int sumaUnaCifra(ArrayList<Integer> ar) {
+    public static int sumaUnaCifra(ArrayList<Integer> arrList) {
         int suma = 0;
 
-        for (int i = 0; i < ar.size(); i++) {
-            if (ar.get(i) < 10) {
-                suma += ar.get(i);
+        for (int i = 0; i < arrList.size(); i++) {
+            if (arrList.get(i) < 10) {
+                suma += arrList.get(i);
             }
         }
         return suma;
     }
 
     /**
-     * Metodo para sumar los Integers mayores o igual a 10 de un ArrayList
+     * Metodo para sumar los enteros mayores o igual a 10 de un ArrayList
      *
-     * @param ar ArrayList de Integers declarado previamente
+     * @param arrList ArrayList de Integers declarado previamente
      * @return Suma de los integers
      */
-    public static int sumaDosCifras(ArrayList<Integer> ar) {
+    public static int sumaDosCifras(ArrayList<Integer> arrList) {
         int suma = 0;
 
-        for (int i = 0; i < ar.size(); i++) {
-            if (ar.get(i) >= 10) {
-                suma += ar.get(i);
+        for (int i = 0; i < arrList.size(); i++) {
+            if (arrList.get(i) >= 10) {
+                suma += arrList.get(i);
             }
         }
         return suma;
--- AccesoDatos/ProyectoAD/src/ProyectoAD/tokenSystemIn.java
+++ AccesoDatos/ProyectoAD/src/ProyectoAD/tokenSystemIn.java
@@ -1,65 +1,65 @@
-package ProyectoAD;
-
-import Herramientas.herramientas;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-import java.util.StringTokenizer;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-
-/**
- *
- * @author Alumnot
- */
-public class tokenSystemIn 
-{
-
-    public static void main(String args[]) 
-    {
-        try {
-            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter escribir = newBufferedWriter(new FileWriter("C:\\Users\\Alumnot\\Documents\\tokenteclado.txt"));
-
-            System.out.println("La primera fila es: num@num");
-            System.out.println("La siguientes: @num@num");
-            for (int i = 0; i < 2; i++) 
-            {
-                if (i == 0) 
-                {
-                    System.out.println("La primera fila: num@num");                    
-                }
-                StringTokenizer st = new StringTokenizer(teclado.readLine(), "@");
-                while (st.hasMoreTokens()) 
-                {
-                    escribir.write(st.nextToken());
-                }
-            }
-            escribir.close();
-
-            BufferedReader leer = new BufferedReader(new FileReader("C:\\Users\\Alumnot\\Documents\\tokenteclado.txt"));
-            String linea;
-            while ((linea = leer.readLine()) != null) 
-            {
-                StringTokenizer st = new StringTokenizer(linea, "=;");
-            }
-        }
-    }
-}
-
-/* Crear fichero token por teclado
-BFR teclado=new BFR(ISR(System.in)
-BFW ESCRIBIR = newBFW(FW("......txt"))
-	for (int i=0; i<2; i++){
-		sout("La primera fila: num@num");
-		sout("La siguientes: @num@num");
-	stokenizer st=new st(teclado.readLine(),"@");
-		while(st.hashMoreTokens()){
-			Escribir.write(st.nextToken());
-		}
-	}
-	es.close();
-*/
+//package ProyectoAD;
+//
+//import Herramientas.herramientas;
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.Scanner;
+//import java.util.StringTokenizer;
+//
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.InputStreamReader;
+//
+///**
+// *
+// * @author Alumnot
+// */
+//public class tokenSystemIn 
+//{
+//
+//    public static void main(String args[]) 
+//    {
+//        try {
+//            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+//            BufferedWriter escribir = newBufferedWriter(new FileWriter("C:\\Users\\Alumnot\\Documents\\tokenteclado.txt"));
+//
+//            System.out.println("La primera fila es: num@num");
+//            System.out.println("La siguientes: @num@num");
+//            for (int i = 0; i < 2; i++) 
+//            {
+//                if (i == 0) 
+//                {
+//                    System.out.println("La primera fila: num@num");                    
+//                }
+//                StringTokenizer st = new StringTokenizer(teclado.readLine(), "@");
+//                while (st.hasMoreTokens()) 
+//                {
+//                    escribir.write(st.nextToken());
+//                }
+//            }
+//            escribir.close();
+//
+//            BufferedReader leer = new BufferedReader(new FileReader("C:\\Users\\Alumnot\\Documents\\tokenteclado.txt"));
+//            String linea;
+//            while ((linea = leer.readLine()) != null) 
+//            {
+//                StringTokenizer st = new StringTokenizer(linea, "=;");
+//            }
+//        }
+//    }
+//}
+//
+///* Crear fichero token por teclado
+//BFR teclado=new BFR(ISR(System.in)
+//BFW ESCRIBIR = newBFW(FW("......txt"))
+//	for (int i=0; i<2; i++){
+//		sout("La primera fila: num@num");
+//		sout("La siguientes: @num@num");
+//	stokenizer st=new st(teclado.readLine(),"@");
+//		while(st.hashMoreTokens()){
+//			Escribir.write(st.nextToken());
+//		}
+//	}
+//	es.close();
+//*/
