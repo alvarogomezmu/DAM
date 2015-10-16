3cecba45d02c70786ad08f2b35866124c7a1d738
alvarogomezmu <alvarog.m@hotmail.com>
2015-10-16 21:14:50 +0200

    sudogudo

--- AccesoDatos/RepasoTema1/RepasoTema1/src/repasotema1/ejercicio2.java
+++ AccesoDatos/RepasoTema1/RepasoTema1/src/repasotema1/ejercicio2.java
@@ -1,14 +1,38 @@
 /*
-Crea un fichero de texto con el nombre y contenido que tu quieras. 
-Ahora crea una aplicación que lea este fichero de texto carácter a carácter y muestre su contenido por pantalla sin espacios. 
-Por ejemplo, si un fichero tiene el siguiente texto “Esto es una prueba”, deberá mostrar “Estoesunaprueba”.
+ Crea un fichero de texto con el nombre y contenido que tu quieras. 
+ Ahora crea una aplicación que lea este fichero de texto carácter a carácter y muestre su contenido por pantalla sin espacios. 
+ Por ejemplo, si un fichero tiene el siguiente texto “Esto es una prueba”, deberá mostrar “Estoesunaprueba”.
  */
 package repasotema1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
- * @author Alumnot
+ * @author AlvaroGomez
  */
 public class ejercicio2 {
-    
+
+    public static void main(String args[]) {
+        try {
+            BufferedReader fichero = new BufferedReader(new FileReader("C:\\Users\\Alumnot\\Desktop\\porejemplo.txt"));
+            char charac[] = new char[288];
+            int count;
+
+            while ((count = fichero.read(charac)) != -1) {
+                for (int i = 0; i < count; i++) {
+                    if (charac[i] != ' ') {
+                        System.out.print(charac[i]);
+                    }
+                }
+            }
+        } catch (FileNotFoundException f) {
+            System.out.println("Archivo no encontrado: " + f);
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+        }
+    }
 }
--- AccesoDatos/RepasoTema1/RepasoTema1/src/repasotema1/ejercicio3.java
+++ AccesoDatos/RepasoTema1/RepasoTema1/src/repasotema1/ejercicio3.java
@@ -1,14 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+En un fichero de acceso aleatorio se han escrito una serie de registros compuestos por dos campos enteros (int, 4 bytes) y uno real en doble precisión (double, 8 bytes).
+Sobre el fichero se aplica el método seek de la forma siguiente: fichero.seek(48).
+
+Si el contenido del fichero es:
+
+        1 5 900.2
+        2 23 1100.4
+        3 2 2000.5
+        4 49 1529.8
+
+
+donde cada línea representa un registro, ¿qué valores se leerían al hacer fichero.readInt() seguido de fichero.readInt() y fichero.readDouble()?
+*/
 package repasotema1;
 
 /**
  *
  * @author Alumnot
  */
+import Herramientas.herramientas;
+import java.util.*;
+import java.lang.*;
 public class ejercicio3 {
     
 }
