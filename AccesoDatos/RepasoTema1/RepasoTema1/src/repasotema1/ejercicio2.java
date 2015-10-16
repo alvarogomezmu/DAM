467b37769f9046ef46140273c7855eec217bdb0a
alvarogomezmu <alvarog.m@hotmail.com>
2015-10-16 20:42:53 +0200

    gfh

---
+++ AccesoDatos/RepasoTema1/RepasoTema1/src/repasotema1/ejercicio2.java
@@ -0,0 +1,14 @@
+/*
+Crea un fichero de texto con el nombre y contenido que tu quieras. 
+Ahora crea una aplicación que lea este fichero de texto carácter a carácter y muestre su contenido por pantalla sin espacios. 
+Por ejemplo, si un fichero tiene el siguiente texto “Esto es una prueba”, deberá mostrar “Estoesunaprueba”.
+ */
+package repasotema1;
+
+/**
+ *
+ * @author Alumnot
+ */
+public class ejercicio2 {
+    
+}
