755cf8956e2ec1c479ab8cfbe74bd46f8d76c5bf
alvarogomezmu <alvarog.m@hotmail.com>
2015-11-10 00:35:16 +0100

    ..

--- SeviciosProcesos/semaforoAscensor/src/semaforoascensor/Persona.java
+++ SeviciosProcesos/semaforoAscensor/src/semaforoascensor/Persona.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    En la clase Persona incluiremos los métodos set y get
  */
 package semaforoascensor;
 
