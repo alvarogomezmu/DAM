a8f94ba7c7f73a1c704500297138ece310ba09b7
alvarogomezmu <alvarog.m@hotmail.com>
2015-12-03 19:58:44 +0100

    calculadoraAWT java

---
+++ DesarrolloInterfaces/calculadoraB/src/calculadorab/Main.java
@@ -0,0 +1,16 @@
+package calculadorab;
+
+/**
+ *
+ * @author Alvaro Gomez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCalculadora calculadora = new VentanaCalculadora();
+        calculadora.setVisible(true);
+    }
+}
