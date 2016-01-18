02329973de8fe085ef916470f8d8db30c7682db8
alvarogomezmu <alvarog.m@hotmail.com>
2016-01-18 19:07:44 +0100

    18-1-2016 acceso a datos ProyectoAD2

---
+++ AccesoDatos/ProyectoAD2/src/Herramientas/Herramientas.java
@@ -0,0 +1,66 @@
+package Herramientas;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.ResourceIterator;
+import org.xmldb.api.base.ResourceSet;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XPathQueryService;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class Herramientas {
+
+    // Metodo para conectar
+    public static void connectExistDB(String uri, String user) {
+        String Driver = "org.exist.xmldb.DatabaseImpl";
+        String URI = "xmldb:exist://localhost:8080/exist/xmlrpc/db/" + uri;
+        String username = user;
+        String password = "root";
+
+        //Proceso de conexion
+        //Crea el canal para conectarsev
+        try {
+            Class C1 = Class.forName(Driver);
+            Database database = (Database) C1.newInstance();
+            DatabaseManager.registerDatabase(database);
+            System.out.println("Inicializado el Driver");
+        } catch (Exception e) {
+            System.out.println("Error al inicializar la Base de Datos");
+        }
+
+        Collection col = null;
+        try {
+            col = DatabaseManager.getCollection(URI, username, password);
+        } catch (Exception ex) {
+
+        }
+        if (col == null) {
+            System.out.println("La coleecion no existe");
+        } else {
+            System.out.println("Te has conectado");
+        }
+        try {
+            XPathQueryService s = (XPathQueryService) col.getService("XPathQueryService", "1.0");
+
+            ResourceSet result = s.query("for $t in //titulo return $t");
+            ResourceIterator i = result.getIterator();
+            while (i.hasMoreResources()) {
+                Resource r = i.nextResource();
+                System.out.println(r.getContent());
+            }
+        } catch (XMLDBException ex) {
+            Logger.getLogger(Herramientas.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void consultaExistDB() {
+
+    }
+}
---
+++ AccesoDatos/ProyectoAD2/src/proyectoad2/ProyectoAD2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoad2;
+
+import Herramientas.Herramientas;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class ProyectoAD2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Herramientas.connectExistDB("prueba", "admin");
+    }
+    
+}
