18e66a5ac5e0a3cfd09464ce709fc8dbfce63563
alvarogomezmu <alvarog.m@hotmail.com>
2016-02-03 20:48:59 +0100

    esto es maravilloso

---
+++ ProgramacionMultimedia/MyApplication/app/src/main/java/com/mutiaso/proyectolista/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mutiaso.proyectolista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// http://jarroba.com/intent-pasar-datos-entre-activity-app-android-video/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
