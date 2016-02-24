7e32ce8b3c383693ec8cb4ffcab66a226281ebd1
alvarogomezmu <alvarog.m@hotmail.com>
2016-02-24 19:14:40 +0100

    calculadoraClienteServidor

--- SeviciosProcesos/calculadoraClienteServidor/src/calculadoraclienteservidor/Cliente.java
+++ SeviciosProcesos/calculadoraClienteServidor/src/calculadoraclienteservidor/Cliente.java
@@ -5,10 +5,49 @@
  */
 package calculadoraclienteservidor;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author AlumnoT
  */
 public class Cliente {
-    
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        double num1;
+        double num2;
+        int operacion = 0;
+        char opr;
+        Socket cliente = new Socket("127.0.0.1", 12342);
+        System.out.println("El cliente se conecta al servidor");
+
+        ObjectInputStream resultado = new ObjectInputStream(cliente.getInputStream());
+        ObjectOutputStream dados = new ObjectOutputStream(cliente.getOutputStream());
+
+        num1 = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el primer número"));
+        num2 = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el segundo número"));
+        while (!((operacion >= 1) && (operacion <= 4))) {
+            operacion = Integer.parseInt(JOptionPane.showInputDialog("¿Que operacion desea realizar? 1= +, 2= -,3= X,4= / "));
+            if (!((operacion >= 1) && (operacion <= 4))) {
+                System.out.println("Operacion no valida");
+            }
+        }
+        dados.writeInt(operacion);
+        dados.writeDouble(num1);
+        dados.writeDouble(num2);
+        dados.flush();
+
+        double total = resultado.readDouble();
+        opr = resultado.readChar();
+        System.out.println("Resultado de " + num1 + opr + num2 + " = " + total);
+
+        resultado.close();
+        dados.close();
+        cliente.close();
+    }
 }
---
+++ SeviciosProcesos/calculadoraClienteServidor/src/calculadoraclienteservidor/Servidor.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoraclienteservidor;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author AlumnoT
+ */
+public class Servidor {
+
+    public static void main(String[] args) throws IOException {
+
+        double num1, num2, total = 0.0;
+        int operacion;
+        char opr = '\n';
+
+        // Crea un socket en el puerto 12342
+        ServerSocket servidor = new ServerSocket(12342);
+        System.out.println("Porta 12342 aberta!");
+
+        // Espera a que alguien se conecte. La ejecucion del servidor se bloquea en la llamada al metodo accept de la clase ServerSocket. 
+        // Cuando alguien se conecta al servidor, el método se desbloquea y devuelve un objeto de la clasea clase de objeto Socket, que es un puerto de comunicación.
+        System.out.print("Esperando a que se conecte el cliente...");
+        Socket cliente = servidor.accept();
+
+        System.out.println("Nueva conexion con el cliente " + cliente.getInetAddress().getHostAddress());
+
+        ObjectOutputStream resultado = new ObjectOutputStream(cliente.getOutputStream());
+        ObjectInputStream dados = new ObjectInputStream(cliente.getInputStream());
+
+        operacion = dados.readInt();
+        num1 = dados.readDouble();
+        num2 = dados.readDouble();
+
+        // Bucle para que el usuario elija una opcion.
+        if (operacion == 1) {
+
+            opr = '+';
+            total = (num1 + num2);
+
+        } else if (operacion == 2) {
+
+            opr = '-';
+            total = (num1 - num2);
+
+        } else if (operacion == 3) {
+
+            opr = 'x';
+            total = (num1 * num2);
+
+        } else {
+
+            opr = '/';
+            total = (num1 / num2);
+
+        }
+
+        resultado.writeDouble(total);
+        resultado.writeChar(opr);
+        resultado.flush();
+
+        resultado.close();
+        dados.close();
+        servidor.close();
+    }
+}
