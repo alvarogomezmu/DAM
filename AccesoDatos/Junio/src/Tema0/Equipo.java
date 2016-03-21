d516b7acc1d590b222ed6540418aaa04d8e5893e
alvarogomezmu <alvarog.m@hotmail.com>
2016-03-21 18:43:40 +0100

    junio

---
+++ AccesoDatos/Junio/src/Tema0/Equipo.java
@@ -0,0 +1,34 @@
+package Tema0;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class Equipo {
+
+    String nombre;
+    Integer cantidad;
+    double precio;
+
+    public void nuevoEquipo(String n, Integer c, double p) {
+        nombre = n;
+        cantidad = c;
+        precio = p;
+    }
+
+    public void descuentoEquipo() {
+        precio = precio * 0.8;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+}
---
+++ AccesoDatos/Junio/src/Tema0/tema0.java
@@ -0,0 +1,102 @@
+/* Crear dos colecciones distintas para poder guardar equipos informáticos (objetos formados por los atributos nombre, cantidad, precio). 
+ En una de las colecciones guardaremos los equipos informáticos cuyo precio no tenga descuento y en la otra los mismo equipos pero con descuento.
+ Una vez hayamos guardado los objetos en cada estructura pertinente. Debemos mostrar el equipo informático con mayor precio y el de menor precio.
+ */
+package Tema0;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ *
+ * @author Alvaro
+ */
+public class tema0 {
+
+    public static void main(String args[]) {
+
+        // Crear 4 equipos de precio normal (nombre,cantidad,precio)
+        Equipo equipo1 = new Equipo();
+        equipo1.nuevoEquipo("Dell", 20, 537);
+        Equipo equipo2 = new Equipo();
+        equipo2.nuevoEquipo("Toshiba", 12, 115);
+        Equipo equipo3 = new Equipo();
+        equipo3.nuevoEquipo("Lenovo", 10, 224);
+        Equipo equipo4 = new Equipo();
+        equipo4.nuevoEquipo("Samsung", 6, 480);
+
+    
+        // Crear copia de cada uno de los 4 equipos
+        Equipo equipo1Desc = new Equipo();
+        equipo1Desc.nuevoEquipo(equipo1.getNombre(), equipo1.getCantidad(), equipo1.getPrecio());
+        Equipo equipo2Desc = new Equipo();
+        equipo2Desc.nuevoEquipo(equipo2.getNombre(), equipo2.getCantidad(), equipo2.getPrecio());
+        Equipo equipo3Desc = new Equipo();
+        equipo3Desc.nuevoEquipo(equipo3.getNombre(), equipo3.getCantidad(), equipo3.getPrecio());
+        Equipo equipo4Desc = new Equipo();
+        equipo4Desc.nuevoEquipo(equipo4.getNombre(), equipo4.getCantidad(), equipo4.getPrecio());
+        
+        // imprimimos los datos de cada equipo
+        System.out.println("equipo 1:\n"+"nombre: "+equipo1.getNombre() +"\ncantidad: "+ equipo1.getCantidad()+ "\nprecio: " +equipo1.getPrecio()+ "\n\n" +"equipo 2:\n"+"nombre: "+equipo2.getNombre()+ "\ncantidad: "+ equipo2.getCantidad()+ "\nprecio:" +equipo2.getPrecio() + "\n\n" +"equipo 3:\n"+"nombre: "+equipo3.getNombre()+ "\ncantidad: "+ equipo3.getCantidad()+ "\nprecio:" +equipo3.getPrecio() + "\n\n" +"equipo 4:\n"+"nombre: "+equipo4.getNombre()+ "\ncantidad: "+ equipo4.getCantidad()+ "\nprecio:" +equipo4.getPrecio() +"\n");
+        
+        // Crear ArrayList para equipos sin descuento
+        ArrayList<Equipo> pc1 = new ArrayList<Equipo>();
+        pc1.add(equipo1);
+        pc1.add(equipo2);
+        pc1.add(equipo3);
+        pc1.add(equipo4);
+        
+        // Crear ArrayList para equipos con descuento
+        ArrayList<Equipo> pc2 = new ArrayList<Equipo>();
+        pc2.add(equipo1Desc);
+        pc2.add(equipo2Desc);
+        pc2.add(equipo3Desc);
+        pc2.add(equipo4Desc);
+        
+        System.out.println("equipo1 con descuento"+equipo1Desc);
+        // Bucle con el que aplicaremos el descuento
+        for (int i = 0; i < pc2.size(); i++) {
+            pc1.get(i).descuentoEquipo();
+        }
+
+        // Declarar variables que se van a usar para comparar y guardar su precio en la posicion y en el ArrayList
+        int menorPc1 = 0;
+        double precioMenorPc1 = 2000;
+        int menorPc2 = 0;
+        double precioMenorPc2 = 2000;
+        int mayorPc1 = 0;
+        double precioMayorPc1 = 0;
+        int mayorPc2 = 0;
+        double precioMayorPc2 = 0;
+
+        // Calcular equipo de la primera lista con menor y mayor precio
+        for (int i = 0; i < pc1.size(); i++) {
+            if (pc1.get(i).getPrecio() > precioMayorPc1) {
+                precioMayorPc1 = pc1.get(i).getPrecio();
+                mayorPc1 = pc1.indexOf(pc1.get(i));
+            }
+        }
+
+        // Calcular equipo de la primera lista con menor y mayor precio
+        for (int i = 0; i < pc2.size(); i++) {
+            if (pc2.get(i).getPrecio() < precioMenorPc2) {
+                precioMenorPc2 = pc2.get(i).getPrecio();
+                menorPc2 = pc2.indexOf(pc2.get(i));
+            }
+        }
+
+        // Comparar precios menores de cada lista y mostrar el menor
+        if (precioMenorPc1 < precioMenorPc2) {
+            System.out.println("Equipo con menor precio: " + pc1.get(menorPc1).getNombre() + " " + pc1.get(menorPc1).getPrecio());
+        } else if (precioMenorPc1 > precioMenorPc2) {
+            System.out.println("Equipo con menor precio: " + pc2.get(menorPc2).getNombre() + " " + pc2.get(menorPc2).getPrecio());
+        }
+
+        // Comparar precios mayores de cada lista y mostrar el mayor
+        if (precioMayorPc1 > precioMayorPc2) {
+            System.out.println("Equipo con mayor precio: " + pc1.get(mayorPc1).getNombre() + " " + pc1.get(mayorPc1).getPrecio());
+        } else if (precioMayorPc1 < precioMayorPc2) {
+            System.out.println("Equipo con mayor precio: " + pc2.get(mayorPc2).getNombre() + " " + pc2.get(mayorPc2).getPrecio());
+        }
+    }
+}
